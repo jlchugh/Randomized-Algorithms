7e5e85248da185393c6e95ad0d102408e20e576e
= <=>
2022-01-04 23:29:25 -0500

    started on quicksort

---
+++ quicksort.py
@@ -0,0 +1,16 @@
+def normal_quicksort(list, left_index, right_index):
+    length = len(list)
+
+    if length == 1:
+        return list
+    if left_index < right_index:
+        middle = length // 2 # the index of the pivot
+        pivot = list[middle]
+
+
+def partition(list, left, right):
+
+
+
+# quicksort that selects a random pivot
+def randomized_quicksort(list):
